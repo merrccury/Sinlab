869f9c7819f515b4fd745798a3a76a13403d249e
Dmitry Alekseichik <merrccury.xz@gmail.com>
2020-10-26 18:07:25 +0300

    Initial Commit

---
+++ src/main/java/by/alekseichik/demo/SinlabApplication.java
@@ -0,0 +1,13 @@
+package by.alekseichik.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinlabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinlabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/alekseichik/demo/SinlabApplicationTests.java
@@ -0,0 +1,13 @@
+package by.alekseichik.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinlabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
